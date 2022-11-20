a77ba6d231d5de1cb4dc5a9f95c640027d2a646e
Limjunhwa <76590302+pasin7793@users.noreply.github.com>
2022-11-20 22:55:13 +0900

    Initial Commit

---
+++ QatarGil/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  QatarGil
+//
+//  Created by 임준화 on 2022/11/20.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
